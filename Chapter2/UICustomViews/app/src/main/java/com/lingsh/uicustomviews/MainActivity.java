22f42fd448122b4bf0bdc11e2bf6e4368729796c
lance-ling <lingshouhong@himobi.net>
2019-12-10 17:50:37 +0800

    Chapter 3 自定义控件 TitleLayout 自定义标题栏 并实现按钮功能 通用

---
+++ Chapter2/UICustomViews/app/src/main/java/com/lingsh/uicustomviews/MainActivity.java
@@ -0,0 +1,20 @@
+package com.lingsh.uicustomviews;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.hide();
+        }
+    }
+}
