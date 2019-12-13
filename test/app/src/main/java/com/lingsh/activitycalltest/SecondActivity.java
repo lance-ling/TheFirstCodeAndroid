ff135e0a158779a9ccb3af5ca4910c95622fe2f5
lance-ling <lingshouhong@himobi.net>
2019-12-13 13:07:03 +0800

    Test startActivity() 的使用方式 它只是将要跳转的意图intent加入任务列表 并不是及时响应跳转

---
+++ test/app/src/main/java/com/lingsh/activitycalltest/SecondActivity.java
@@ -0,0 +1,62 @@
+package com.lingsh.activitycalltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "SecondActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Log.d(TAG, "onCreate: ");
+
+        Button tran2other = findViewById(R.id.tran2other);
+        tran2other.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+}
